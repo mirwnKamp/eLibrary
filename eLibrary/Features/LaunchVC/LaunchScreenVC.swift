e66ab357e17f790d9435fdd28149e6761cf1ee6c
Myron Kampourakis <kampourakis.murwn@gmail.com>
2024-07-19 00:29:30 +0300

    Add lottie animation on LaunchScreenVc

--- eLibrary/Features/LaunchVC/LaunchScreenVC.swift
+++ eLibrary/Features/LaunchVC/LaunchScreenVC.swift
@@ -6,11 +6,27 @@
 //
 
 import UIKit
+import Lottie
 
 class LaunchScreenVC: UIViewController, Coordinator {
     
+    private var animationView = LottieAnimationView.newAutoLayoutView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigate(NavigationItem(page: .main, navigationStyle: .push(animated: false)))
+        view.addSubview(animationView)
+        animationView.backgroundColor = .white
+        animationView.fillToSuperview()
+        
+
+        animationView.animation = LottieAnimation.named("booksAnimation")
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        
+        animationView.play()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.3) {
+            self.navigate(NavigationItem(page: .main, navigationStyle: .push(animated: true)))
+        }
+
     }
 }
